4e4f6073c02a4f75b9669e267d34b4f883041621
charlieguo <zixin.guo20@ycdsbk12.ca>
2020-01-20 21:26:00 -0500

    all_clear

--- class_learning.py
+++ class_learning.py
@@ -1,62 +1,42 @@
+enemy_list = [2, 2, 4 ,2, 1, 2, 0, 10]
+def sort_enemy(enemy_list):
+    if len(enemy_list) == 0:
+        return []
+    if len(enemy_list) == 1:
+        return enemy_list
+    if len(enemy_list) == 2:
+        if enemy_list[0] > enemy_list[1]:
+            return [enemy_list[0], enemy_list[1]]
+        return [enemy_list[1], enemy_list[0]]
+    return sort_enemy([ele for ele in enemy_list[1:] if ele >= enemy_list[0]]) + [enemy_list[0]] + sort_enemy(
+        [ele for ele in enemy_list[1:] if ele < enemy_list[0]])
+# print(sort_enemy(enemy_list))
+W = 25
+points = [1, 2, 1, 2, 1, 2, 3, 2, 1]
+n = len(points)
+ehps = [5, 5, 5, 5, 5, 5, 5, 5, 5]
+dic = {}
+m = []
+def opti_missile(W, n):
+    if n == 0 or W == 0:
+        return 0
+    key = str(W) + "#" + str(n)
 
-class Human:
-    # Human_left_arm : "Human_arm"
-    # Human_right_arm : "Human_arm"
-    # Human_body : "Human_body"
-    # Human_left_leg : "Human_leg"
-    # Human_right_leg : "Human_leg"
+    if key in dic:
+        return dic[key]
 
+    if (ehps[n - 1] > W):
+        dic[key] = opti_missile(W, n - 1)
+        return dic[key]
 
-    def __init__(self):
-        self.Human_body = Human_body()
-        self.Human_body.setposition(5, 5)
+    else:
+        previous = opti_missile(W - ehps[n - 1], n - 1)
+        previous_not = opti_missile(W, n - 1)
+        dic[key] = max(points[n - 1] + previous,
+                      previous_not)
+        m.append(dic[key])
+        return dic[key]
+# 11?
+print(opti_missile(W, n))
+print(m[-1])
 
-        self.Human_left_arm = Human_arm()
-        self.Human_left_arm.setposition(5,0)
-
-        self.Human_right_arm = Human_arm()
-        self.Human_right_arm.setposition(5,10)
-
-        self.Human_left_leg = Human_leg()
-        self.Human_left_leg.setposition(0,0)
-
-        self.Human_right_leg = Human_leg()
-        self.Human_right_leg.setposition(0,10)
-
-    def print_position(self):
-        print(self.Human_body.x)
-        print(self.Human_body.y)
-
-
-
-
-
-
-
-class Human_component:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-
-    def setposition(self, x, y):
-        self.x = x
-        self.y = y
-
-class Human_arm(Human_component):
-
-    def __init__(self):
-        super().__init__()
-
-class Human_body(Human_component):
-
-    def __init__(self):
-        super().__init__()
-
-class Human_leg(Human_component):
-
-    def __init__(self):
-        super().__init__()
-
-if __name__ == "__main__":
-    tom = Human()
-    tom.print_position()
